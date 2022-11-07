8d0d4149794b744aab9d871eb1c7a8119e31dcee
Mohand Abdo <mohand30466@gmail.com>
2022-11-07 12:46:04 +0200

    add to do list url

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import  BussinessStaffViews, BussinessUsViews, HoursViews, InvoicesViews, ShiftViews, UserViews, UserProfileViews,PostViews, LikeViews,PokeViews,CommentsViews,MessageViews,CustomAuthToken,PlogPostcommetsViews,PlogPostViews,ContactUsViews,PaysleeveViews
+from .views import  BussinessStaffViews,ToDoListViews, BussinessUsViews, HoursViews, InvoicesViews, ShiftViews, UserViews, UserProfileViews,PostViews, LikeViews,PokeViews,CommentsViews,MessageViews,CustomAuthToken,PlogPostcommetsViews,PlogPostViews,ContactUsViews,PaysleeveViews
 
 router = routers.DefaultRouter()
 router.register(r"users",UserViews)
@@ -19,6 +19,7 @@ router.register(r"shift",ShiftViews)
 router.register(r"card",HoursViews)
 router.register(r"invoice",InvoicesViews)
 router.register(r"paysleeve",PaysleeveViews)
+router.register(r"todolist",ToDoListViews)
 
 
 
