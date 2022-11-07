4f163d1c5e48c94bede2f04810fe43ebad6e9da6
Mohand Abdo <mohand30466@gmail.com>
2022-11-07 20:03:23 +0200

    remove field

--- user/seializers.py
+++ user/seializers.py
@@ -1,7 +1,7 @@
 from importlib.metadata import requires
 from pyexpat import model
 from rest_framework.authtoken.models import Token
-from .models import Bussines,ToDoList,Invoice, BussinesStaff, ContactUs, HoursCard, Invoices, Post, Shift, Userprofile,likes,Pokes,Comments,Messages,PlogPost,PlogPostComments,Paysleeve
+from .models import Bussines,ToDoList, BussinesStaff, ContactUs, HoursCard, Invoices, Post, Shift, Userprofile,likes,Pokes,Comments,Messages,PlogPost,PlogPostComments,Paysleeve
 
 from rest_framework import serializers
 from .models import User
@@ -116,16 +116,9 @@ class HoursSerializer(serializers.ModelSerializer):
 class InvoicesSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Invoices
-        fields= ['id',"bussiness","invoiceType","issueAt","reciverName","reciverId","reciverEmail","invoiceDetail","invoiceAmount","invoiceTax","paymentTill"]
+        fields= ['id',"bussiness","invoiceType"]
     
-class InvoiceSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Invoice
-        fields= ['id',"bussiness","invoiceType","issueAt","reciverName","reciverId","reciverEmail","invoiceDetail","invoiceAmount","invoiceTax","paymentTill"]
-    
-  
-    
-    
+
 class PaysleeveSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Paysleeve
