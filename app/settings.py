009926b114548ba57cd2bf8fa5035dd8f236e94d
Mohand Abdo <mohand30466@gmail.com>
2022-12-08 15:45:40 +0200

    reset data base

--- app/settings.py
+++ app/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 # default_dburl = 'sqlite:///'+ os.path.join(BASE_DIR,'db.sqlite3')
-# default=default_dburl,cast=dburl
 
 DATABASES = {
-    'default': config('DATABASE_URL'),
+    'default': config('DATABASE_URL',cast=dburl),
 }
 
 
